26447c987a9437c4adef93a67f0efe769b5344ae
Braulio Monroy <brauliome98@gmail.com>
2020-06-13 04:36:31 -0500

    holas

---
+++ MerxProject/Models/PruebaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MerxProject.Models
+{
+    public class PruebaModel
+    {
+        public void hola()
+        {
+
+        }
+    }
+}
