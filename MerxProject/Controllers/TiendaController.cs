0ccbfe57eadd1faa7f74e82ec44a83f6659c55e3
unknown <brauliome98@gmail.com>
2020-08-22 02:09:09 -0500

    magia

--- MerxProject/Controllers/TiendaController.cs
+++ MerxProject/Controllers/TiendaController.cs
@@ -196,6 +196,18 @@ namespace MerxProject.Controllers
         [HttpGet]
         public async Task<ActionResult> PagoProducto(int? dire, string subtotal, string total, string cupon, string ship = "0")
         {
+            var persona = DbModel.Personas.FirstOrDefault(x => x.Correo == User.Identity.Name);
+            var DirExist = DbModel.Direcciones.Where(x => x.IdPersona == persona.idPersona).ToList();
+
+            if (DirExist.Count > 0)
+            {
+                ViewBag.Direxist = 1;
+            }
+            else
+            {
+                ViewBag.Direxist = 0;
+            }
+
             if (string.IsNullOrEmpty(ship))
             {
                 ship = "Gratis";
@@ -205,7 +217,7 @@ namespace MerxProject.Controllers
             using (this.DbModel = new ApplicationDbContext())
             {
 
-                var persona = DbModel.Personas.FirstOrDefault(x => x.Correo == User.Identity.Name);
+                
                 var ordenPendiente = DbModel.Orders.FirstOrDefault(x => x.idPersona == persona.idPersona && x.Estatus == "Procesando");
                 var fromDatabaseEF = new SelectList(DbModel.Direcciones.Where(x => x.IdPersona == persona.idPersona).ToList(), "Id", "DirCalle");
                 var cantidadCarrito = DbModel.CarritoCompras.Where(X => X.idPersona == persona.idPersona).ToList();
